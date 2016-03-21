05a85710f97ced26d7c959169ebe7b77d805c05a
nishanth28 <pnishanth28@gmail.com>
2016-03-21 13:24:53 -0400

    first commit

---
+++ Restaurants/ViewController1.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController1.swift
+//  Restaurants
+//
+//  Created by Nishanth P on 3/18/16.
+//  Copyright © 2016 Nishapp. All rights reserved.
+//
+
+import Foundation
---
+++ Restaurants/ViewController2.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController2.swift
+//  Restaurants
+//
+//  Created by Nishanth P on 3/18/16.
+//  Copyright © 2016 Nishapp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController2: UIViewController {
+
+}
